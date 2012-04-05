ed8551b5e69aafcad036bddbf55a1fbda8e924f9
sebastien.zurfluh <sebastien.zurfluh@gmail.com>
2012-04-05 19:18:24 +0200

    Added some resources and styling.

--- src/ch/sebastienzurfluh/client/view/FooterWidget.java
+++ src/ch/sebastienzurfluh/client/view/FooterWidget.java
@@ -19,8 +19,6 @@
 
 package ch.sebastienzurfluh.client.view;
 
-import ch.sebastienzurfluh.client.control.eventbus.EventBus;
-
 import com.google.gwt.user.client.ui.Label;
 
 /**
@@ -32,6 +30,6 @@ public class FooterWidget extends Label {
 	public FooterWidget() {
 		super("Created by Sébastien Zurfluh for the Swiss Museum");
 		
-		setStyleName("FooterWidget");
+		setStyleName("footerWidget");
 	}
 }
--- src/ch/sebastienzurfluh/client/view/TileMenu/TileWidget.java
+++ src/ch/sebastienzurfluh/client/view/TileMenu/TileWidget.java
@@ -19,8 +19,6 @@
 
 package ch.sebastienzurfluh.client.view.TileMenu;
 
-import java.util.LinkedList;
-
 import ch.sebastienzurfluh.client.control.eventbus.Event;
 import ch.sebastienzurfluh.client.control.eventbus.EventBus;
 import ch.sebastienzurfluh.client.control.eventbus.Event.EventType;
@@ -28,6 +26,8 @@ import ch.sebastienzurfluh.client.control.eventbus.EventBusListener;
 import ch.sebastienzurfluh.client.control.eventbus.events.PageChangeEvent;
 import ch.sebastienzurfluh.client.model.Model;
 import ch.sebastienzurfluh.client.model.structure.Data;
+import ch.sebastienzurfluh.client.model.structure.DataReference;
+import ch.sebastienzurfluh.client.model.structure.MenuData;
 import ch.sebastienzurfluh.client.view.TileMenu.Tile.TileMode;
 
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -43,7 +43,6 @@ public class TileWidget extends VerticalPanel implements EventBusListener {
 	private TileMenu bookletMenu;
 	private TileMenu chapterMenu;
 	private TileMenu pageMenu;
-	private TileMenu resourceMenu;
 	private Model model;
 	
 	public TileWidget(EventBus pageChangeEventBus, Model model) {
@@ -60,13 +59,10 @@ public class TileWidget extends VerticalPanel implements EventBusListener {
 		add(chapterMenu);
 		pageMenu = new TileMenu("Pages");
 		add(pageMenu);
-		resourceMenu = new TileMenu("Resources");
-		add(resourceMenu);
 		
 		// Default settings
 		chapterMenu.setVisible(false);
 		pageMenu.setVisible(false);
-		resourceMenu.setVisible(false);
 	}
 
 	@Override
@@ -81,44 +77,26 @@ public class TileWidget extends VerticalPanel implements EventBusListener {
 			
 			// Change layout according to the new page type.
 			switch (pageChangeEvent.getPageType()) {
-			case RESSOURCE:
-				resourceMenu.setVisible(true);
-				resourceMenu.setMode(TileMode.ICON_ONLY);
-				pageMenu.setVisible(true);
-				pageMenu.setMode(TileMode.ICON_ONLY);
-				chapterMenu.setVisible(true);
-				chapterMenu.setMode(TileMode.ICON_ONLY);
-				bookletMenu.setVisible(true);
-				bookletMenu.setMode(TileMode.ICON_ONLY);
-				break;
 			case PAGE:
-				resourceMenu.setVisible(false);
 				pageMenu.setVisible(true);
 				pageMenu.setMode(TileMode.ICON_ONLY);
 				chapterMenu.setVisible(true);
 				chapterMenu.setMode(TileMode.ICON_ONLY);
-				bookletMenu.setVisible(true);
 				bookletMenu.setMode(TileMode.ICON_ONLY);
 				break;
 			case CHAPTER:
-				resourceMenu.setVisible(false);
 				pageMenu.setVisible(false);
 				chapterMenu.setVisible(true);
-				bookletMenu.setVisible(true);
 				bookletMenu.setMode(TileMode.ICON_ONLY);
 				break;
 			case SUPER:
-				resourceMenu.setVisible(false);
 				pageMenu.setVisible(false);
 				chapterMenu.setVisible(false);
-				bookletMenu.setVisible(true);
 				bookletMenu.setMode(TileMode.DETAILED);
 				break;
 			case BOOKLET:
-				resourceMenu.setVisible(false);
 				pageMenu.setVisible(false);
-				chapterMenu.setVisible(false);
-				bookletMenu.setVisible(true);
+				chapterMenu.setVisible(true);
 				bookletMenu.setMode(TileMode.ICON_ONLY);
 				break;
 			default:
@@ -127,37 +105,25 @@ public class TileWidget extends VerticalPanel implements EventBusListener {
 			
 			// Reload the tiles as necessary
 			Data data = pageChangeEvent.getData();
-			data.getParentIterator();
 			switch (pageChangeEvent.getPageType()) {
-			case RESSOURCE:
-				loadTiles(resourceMenu, data);
-				data = data.getParent();
-			case PAGE:
-				loadTiles(pageMenu, dataId);
-				dataId = model.getParent(dataId);
-			case CHAPTER:
-				loadTiles(chapterMenu, dataId);
-				dataId = model.getParent(dataId);
 			case BOOKLET:
-				loadTiles(bookletMenu, dataId);
-				dataId = model.getParent(dataId);
-			case SUPER:
+				// list the booklet's chapters
+				reloadTiles(chapterMenu, data.getReference());
+				break;
+			case CHAPTER:
+				// list the chapter's pages
+				reloadTiles(pageMenu, data.getReference());
+				break;
 			default:
 				break;
 			}
 		}
 	}
 	
-	private void loadTiles(TileMenu menu, int pageId) {
-		// Delete old tiles.
+	private void reloadTiles (TileMenu menu, DataReference parentReference) {
 		menu.clearTiles();
-		
-		// Add new tiles.
-		LinkedList<Integer> siblingsIds = model.getSiblings(pageId);
-		for (Integer id : siblingsIds) {
-			Data data = model.getData(id);
-			menu.addTile(data.getSquareImgURL(), data.getTitle(), data.getDescription(), data.getPriorityNumber());
+		for (MenuData menuData : model.getSubMenus(parentReference.getType(), parentReference)) {
+			menu.addTile(menuData.getSquareImgURL(), menuData.getTitle(), menuData.getDescription(), menuData.getPriorityNumber());
 		}
 	}
-
 }
