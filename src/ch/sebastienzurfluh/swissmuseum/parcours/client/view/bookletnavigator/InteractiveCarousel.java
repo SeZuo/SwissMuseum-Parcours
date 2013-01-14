118554593508b20d741f92fecf6d1ad151f1e980
sebastien.zurfluh <sebastien.zurfluh@gmail.com>
2013-01-14 10:44:16 +0100

    Updated GPLv3 headers to include 2013

--- src/ch/sebastienzurfluh/swissmuseum/parcours/client/view/bookletnavigator/InteractiveCarousel.java
+++ src/ch/sebastienzurfluh/swissmuseum/parcours/client/view/bookletnavigator/InteractiveCarousel.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012-2013 Sebastien Zurfluh
+ * 
+ * This file is part of "Parcours".
+ * 
+ * "Parcours" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * "Parcours" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with "Parcours".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ch.sebastienzurfluh.swissmuseum.parcours.client.view.bookletnavigator;
 
 import ch.sebastienzurfluh.swissmuseum.core.client.control.eventbus.EventBus;
@@ -30,21 +49,22 @@ public class InteractiveCarousel extends Carousel {
 					previousSelection = -1;
 					eventBus.fireEvent(new PageChangeRequest(DataReference.SUPER));
 					return;
-				}
-				
-				if (previousSelection != -1 && previousSelection < event.getSelectedItem()) {
+				} else {
+
+					if (previousSelection != -1 && previousSelection < event.getSelectedItem()) {
+						previousSelection = event.getSelectedItem();
+						eventBus.fireEvent(new PageChangeRequest(model.getNextPageMenu().getReference()));
+						return;
+					}
+
+					if (previousSelection > event.getSelectedItem()) {
+						previousSelection = event.getSelectedItem();
+						eventBus.fireEvent(new PageChangeRequest(model.getPreviousPageMenu().getReference()));
+						return;
+					}
+					
 					previousSelection = event.getSelectedItem();
-					eventBus.fireEvent(new PageChangeRequest(model.getNextPageMenu().getReference()));
-					return;
 				}
-				
-				if (previousSelection > event.getSelectedItem()) {
-					previousSelection = event.getSelectedItem();
-					eventBus.fireEvent(new PageChangeRequest(model.getPreviousPageMenu().getReference()));
-					return;
-				}
-				
-				previousSelection = event.getSelectedItem();
 			}
 		});
 	}
